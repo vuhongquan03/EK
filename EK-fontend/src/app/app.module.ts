350642c374b510a5946745e4371c8c21c5f528c9
quanvh <quanvh@d2t.vn>
2019-08-14 11:23:36 +0700

    second

---
+++ EK-fontend/src/app/app.module.ts
@@ -0,0 +1,33 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
+import { ComponentComponent } from './component/component.component';
+import { HeaderComponent } from './component/header/header.component';
+import { BodyComponent } from './component/body/body.component';
+import { FooterComponent } from './component/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponentComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+    

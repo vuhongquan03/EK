dd073ec430b58a96c3bf2dd7409af18f9412fc57
quanvh <quanvh@d2t.vn>
2019-08-14 11:15:03 +0700

    f

---
+++ src/main/java/com/EK/EK/EkApplication.java
@@ -0,0 +1,13 @@
+package com.EK.EK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkApplication.class, args);
+	}
+
+}
